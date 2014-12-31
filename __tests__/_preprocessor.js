23dd09277e7871d0803d67664ff34fc660a97b52
Alec Perkins <alecperkins@gmail.com>
2014-12-31 11:38:23 -0500

    Add testing using Jest, with support for CoffeeScript/CJSX tests.
    The tests can be run using `npm test`. JavaScript tests are currently not supported since .js files are ignored (except for the Jest preprocessor).

---
+++ __tests__/_preprocessor.js
@@ -0,0 +1,15 @@
+// preprocessor.js
+var coffee = require('coffee-script');
+var coffeereact = require('coffee-react');
+
+module.exports = {
+    process: function(src, path) {
+        if (path.match(/\.coffee$/)) {
+            return coffee.compile(src, {bare: true});
+        }
+        if (path.match(/\.cjsx$/)) {
+            return coffeereact.compile(src, {bare: true});
+        }
+        return src;
+    }
+};
