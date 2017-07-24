6a865410a5710c7b0330c567d70865a8a6997cb6
Alex Cabrera <alex@droptype.com>
2017-07-24 13:00:31 -0400

    add support for section breaks

--- entry/index.js
+++ entry/index.js
@@ -3,6 +3,7 @@ module.exports = {
     ImageBlock                  : require('./ImageBlock'),
     GalleryBlock                : require('./GalleryBlock'),
     ListBlock                   : require('./ListBlock'),
+    SectionBreakBlock           : require('./SectionBreakBlock'),
     TextBlock                   : require('./TextBlock'),
     renderEntryContent          : require('./renderEntryContent'),
     renderEntryContentToString  : require('./renderEntryContentToString'),
